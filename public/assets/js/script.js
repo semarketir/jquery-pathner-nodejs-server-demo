d52d07724728a9c43aeb4eede71d7f11c38bfb80
Jani <jani@path.com>
2016-09-23 11:27:30 +0700

    update template

---
+++ public/assets/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('.path-share-button').pathPartner({
+        clientId: '63c2dd2e4c395ae09fa013ea3c1f9bf6392b19f4', // insert your client id in here
+        appname: 'jQuery Pathner Demo', // insert your app name in here
+        action: '/post-moment', // url for post moment on server
+        method: 'post', // let's set post as a method
+        data: function (self) {
+            return {
+                url: window.location.href,
+                image: $('#image_preview').attr('src'),
+                title: $(self).data('title'),
+                description: $('#preview_description').text()
+            };
+        },
+
+        // optional
+        callback: {
+            success: function (data, done) {
+                console.log(data);
+                done();
+            },
+            error: function (xhr, status, err) {
+                console.error(status, err.toString());
+            }
+        }
+    });
+});
